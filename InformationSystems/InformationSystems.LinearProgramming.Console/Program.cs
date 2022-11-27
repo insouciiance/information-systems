dbe9ca7f1829e3c0b8a24140efddc86079812bb3
Mykyta Kovalenko <nikkv.ottf@gmail.com>
2022-11-27 12:52:18 +0100

    touch-ups for simplex method

--- InformationSystems/InformationSystems.LinearProgramming.Console/Program.cs
+++ InformationSystems/InformationSystems.LinearProgramming.Console/Program.cs
@@ -1,4 +1,5 @@
-﻿using InformationSystems.LinearProgramming;
+﻿using System.Text;
+using InformationSystems.LinearProgramming;
 
 //var tableau = SimplexTableau<double>.CreateBuilder()
 //	.SetTargetFunction(new SimplexRow<double>(1, -2, 0, 1, -5, 0, 2))
@@ -9,13 +10,54 @@
 
 var tableau = SimplexTableau<double>.CreateBuilder()
 	.SetTargetFunction(new SimplexRow<double>(1, 2, 2, 0, 0, 0, 3))
-	.AddRow(new SimplexRow<double>(0, 5, 2, 0, 0, 0, 1))
-	.AddRow(new SimplexRow<double>(0, 3, 2, 0, 0, 0, 0))
-	.AddRow(new SimplexRow<double>(0, 3, 3, 0, 0, 0, 2))
+	.AddRow(new SimplexRow<double>(0, 5, 2, 0, 1, 0, 1))
+	.AddRow(new SimplexRow<double>(0, 3, 2, 0, 0, 1, 0))
+	.AddRow(new SimplexRow<double>(0, 3, 3, 1, 0, 0, 2))
 	.ToTableau();
-	
 
 SimplexTableauProcessor<double> processor = new(tableau);
+
+processor.IterationStart += (t, _) => PrintTableau(t);
+processor.MaxEntryFound += (_, entry, i) => Console.WriteLine($"Found entry: {Round(entry)}, j: {i}\n");
+processor.PivotFound += (_, pivot, j) => Console.WriteLine($"Pivot: {Round(pivot)}, i: {j}\n");
+processor.RunFinished += PrintTableau;
+
 double result = processor.RunSimplex(false);
 
-Console.WriteLine(result);
+Console.WriteLine(Round(result));
+
+void PrintTableau(SimplexTableau<double> tableau)
+{
+	StringBuilder builder = new();
+
+	for (int i = 0; i < tableau.TargetFunction.VariableCoefficients.Length; i++)
+		builder.Append($"{$"x{i + 1}",-6}");
+
+	builder.Append($"{"Z",-6}");
+	builder.Append($"{"|",-6}");
+	builder.Append($"{"C",-6}");
+
+	builder.AppendLine();
+
+	foreach (var current in tableau.Rows)
+		AppendRow(current);
+
+	builder.AppendLine(new string('-', (tableau.TargetFunction.VariableCoefficients.Length + 3) * 5));
+
+	AppendRow(tableau.TargetFunction);
+
+	Console.WriteLine(builder.ToString());
+
+	void AppendRow(SimplexRow<double> row)
+	{
+		foreach (var c in row.VariableCoefficients)
+			builder.Append($"{Round(c),-6}");
+
+		builder.Append($"{Round(row.Z),-6}");
+		builder.Append($"{"|",-6}");
+		builder.Append($"{Round(row.C),-6}");
+		builder.AppendLine();
+	}
+}
+
+double Round(double d) => Math.Round(d, 2);
--- InformationSystems/InformationSystems.LinearProgramming/SimplexTableauProcessor.cs
+++ InformationSystems/InformationSystems.LinearProgramming/SimplexTableauProcessor.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Numerics;
 
 namespace InformationSystems.LinearProgramming;
@@ -7,6 +8,16 @@ public class SimplexTableauProcessor<T>
 	where T : INumber<T>
 {
 	private SimplexTableau<T> _tableau;
+
+	public event Action<SimplexTableau<T>, int>? IterationStart;
+
+	public event Action<SimplexTableau<T>, T, int>? MaxEntryFound;
+
+	public event Action<SimplexTableau<T>, T, int>? PivotFound;
+
+	public event Action<SimplexTableau<T>>? TableauNormalized;
+
+	public event Action<SimplexTableau<T>>? RunFinished; 
 
 	public SimplexTableauProcessor(SimplexTableau<T> tableau)
 	{
@@ -21,89 +32,99 @@ public class SimplexTableauProcessor<T>
 	private T RunSimplexInternal<TOptimizer>()
 		where TOptimizer : IOptimizer
 	{
-		while (!CheckFinished())
+		for(int i = 0; !CheckFinished(); i++)
 		{
-			var (entry, x) = FindMaxEntry();
+			IterationStart?.Invoke(_tableau, i);
+
+			var (entry, x) = FindMaxEntry<TOptimizer>();
+			MaxEntryFound?.Invoke(_tableau, entry, x);
+
 			var (pivot, y) = FindPivot(x);
+			PivotFound?.Invoke(_tableau, pivot, y);
+
 			_tableau = NormalizeTableau(pivot, x, y);
+			TableauNormalized?.Invoke(_tableau);
 		}
+
+		RunFinished?.Invoke(_tableau);
 
 		return _tableau.TargetFunction.C;
 
 		bool CheckFinished() => _tableau.TargetFunction.VariableCoefficients.All(c => !TOptimizer.ShouldOptimize(c));
+	}
 
-		(T Entry, int Index) FindMaxEntry()
+	private (T Entry, int Index) FindMaxEntry<TOptimizer>()
+		where TOptimizer : IOptimizer
+	{
+		T maxEntry = _tableau.TargetFunction.VariableCoefficients[0];
+		int index = 0;
+
+		for (int i = 1; i < _tableau.TargetFunction.VariableCoefficients.Length; i++)
 		{
-			T maxEntry = _tableau.TargetFunction.VariableCoefficients[0];
-			int index = 0;
+			T current = _tableau.TargetFunction.VariableCoefficients[i];
 
-			for (int i = 1; i < _tableau.TargetFunction.VariableCoefficients.Length; i++)
+			if (TOptimizer.Compare(maxEntry, current) < T.Zero)
 			{
-				T current = _tableau.TargetFunction.VariableCoefficients[i];
-
-				if (TOptimizer.Compare(maxEntry, current) < T.Zero)
-				{
-					maxEntry = current;
-					index = i;
-				}
+				maxEntry = current;
+				index = i;
 			}
-
-			return (maxEntry, index);
 		}
 
-		(T Pivot, int Index) FindPivot(int x)
+		return (maxEntry, index);
+	}
+
+	private (T Pivot, int Index) FindPivot(int x)
+	{
+		int index = -1;
+		T pivot = default!;
+		T quotient = default!;
+
+		for (int i = 0; i < _tableau.Rows.Length; i++)
 		{
-			int index = -1;
-			T pivot = default!;
-			T quotient = default!;
+			SimplexRow<T> row = _tableau.Rows[i];
 
-			for (int i = 0; i < _tableau.Rows.Length; i++)
+			T current = row.VariableCoefficients[x];
+
+			if (current < T.Zero)
+				continue;
+
+			T currentQuotient = row.C / current;
+
+			if (index == -1 || currentQuotient < quotient)
 			{
-				SimplexRow<T> row = _tableau.Rows[i];
-
-				T current = row.VariableCoefficients[x];
-
-				if (current < T.Zero)
-					continue;
-
-				T currentQuotient = row.C / current;
-
-				if (index == -1 || currentQuotient < quotient)
-				{
-					pivot = current;
-					quotient = currentQuotient;
-					index = i;
-				}
+				pivot = current;
+				quotient = currentQuotient;
+				index = i;
 			}
-
-			return (pivot, index);
 		}
 
-		SimplexTableau<T> NormalizeTableau(T pivot, int x, int y)
+		return (pivot, index);
+	}
+
+	private SimplexTableau<T> NormalizeTableau(T pivot, int x, int y)
+	{
+		var builder = _tableau.ToBuilder();
+
+		SimplexRow<T> pivotRow = _tableau.Rows[y];
+		builder.SetRow(pivotRow * (T.One / pivot), y);
+
+		builder.SetTargetFunction(NormalizeRow(_tableau.TargetFunction));
+
+		for (int i = 0; i < _tableau.Rows.Length; i++)
 		{
-			var builder = _tableau.ToBuilder();
+			if (i == y)
+				continue;
 
-			SimplexRow<T> pivotRow = _tableau.Rows[y] * (T.One / pivot!);
-			builder.SetRow(pivotRow, y);
+			SimplexRow<T> current = _tableau.Rows[i];
+			builder.SetRow(NormalizeRow(current), i);
+		}
 
-			builder.SetTargetFunction(NormalizeRow(_tableau.TargetFunction));
+		return builder.ToTableau();
 
-			for (int i = 0; i < _tableau.Rows.Length; i++)
-			{
-				if (i == y)
-					continue;
-
-				SimplexRow<T> current = _tableau.Rows[i];
-				builder.SetRow(NormalizeRow(current), i);
-			}
-
-			return builder.ToTableau();
-
-			SimplexRow<T> NormalizeRow(SimplexRow<T> row)
-			{
-				T factor = row.VariableCoefficients[x] / -pivot!;
-				return pivotRow * factor + row;
-			}
+		SimplexRow<T> NormalizeRow(SimplexRow<T> row)
+		{
+			T factor = -row.VariableCoefficients[x] / pivot;
+			return pivotRow * factor + row;
 		}
 	}
 
